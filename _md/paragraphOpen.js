9c089d860dd073a1df08492d678346108d042fd7
Christian LeVesque <christian@christianlevesque.io>
2020-11-30 14:09:36 -0600

    Removed console.log

--- _md/paragraphOpen.js
+++ _md/paragraphOpen.js
@@ -5,7 +5,7 @@ const defaultRender = md.renderer.rules.paragraph_open || defaultRenderer
 module.exports = function (tokens, idx, options, env, self) {
 	// Get the token
 	const token  = tokens[idx]
-	console.log(token)
+
 	// Add Bootstrap classes
 	token.attrPush(["class", "col-12 my-2"])
 
