7667bf709f90e8d856fd0e4be399471b5f96342b
Christian LeVesque <christian@christianlevesque.io>
2020-12-01 19:06:42 -0600

    switched scroll-to-top.js over to jquery

--- js/utilities/scroll-to-top.js
+++ js/utilities/scroll-to-top.js
@@ -1,10 +1,8 @@
+import $ from "jquery"
 const fab = document.getElementById("scroll-to-top")
 
 if (fab) {
 	fab.addEventListener("click", () => {
-		window.scroll({
-			top: 0,
-			behavior: "smooth"
-		})
+		$("html, body").animate({scrollTop: 0}, "slow")
 	})
 }
