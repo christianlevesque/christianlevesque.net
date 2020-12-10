3ee6dbb169fb0406b80df0e4da71ab13fb83870b
Christian LeVesque <christian@christianlevesque.io>
2020-12-10 12:57:49 -0600

    Changed deployment to only copy new or modified files and to delete old files, to avoid accidentally busting the Nginx cache on the blog server

--- deploy.sh
+++ deploy.sh
@@ -6,23 +6,21 @@ printf "...website built.\n"
 
 printf "Creating archive for deployment...\n"
 find ./build -name *.map -prune -o -type f -print0 | xargs -0 tar -rf blog.tar
+tar -rf blog.tar copyblog.sh
 printf "...archive created.\n"
 
 printf "Deploying archive to server...\n"
 scp blog.tar $CL_BLOG_CREDENTIAL:
 printf "...archive deployed.\n"
 
-printf "Clearing previous build from server...\n"
-ssh $CL_BLOG_CREDENTIAL "rm -r $CL_BLOG_DIRNAME"
-printf "...previous build cleared.\n"
-
 printf "Extracting archive on server...\n"
-ssh $CL_BLOG_CREDENTIAL "mkdir $CL_BLOG_DIRNAME"
 ssh $CL_BLOG_CREDENTIAL "tar -xf blog.tar"
-ssh $CL_BLOG_CREDENTIAL "mv build $CL_BLOG_DIRNAME/public"
+ssh $CL_BLOG_CREDENTIAL "/usr/bin/env bash copyblog.sh"
 printf "...archive extracted.\n"
 
 printf "Cleaning up deployment...\n"
 ssh $CL_BLOG_CREDENTIAL "rm blog.tar"
+ssh $CL_BLOG_CREDENTIAL "rm -r build"
+ssh $CL_BLOG_CREDENTIAL "rm copyblog.sh"
 rm blog.tar
 printf "...deployment cleaned.\n"
