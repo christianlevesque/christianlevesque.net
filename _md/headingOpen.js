8c6158b7855e22e467cf7e62bec8c5af6519f974
Christian LeVesque <christian@christianlevesque.io>
2020-12-10 16:43:27 -0600

    Added border-bottom, border-dark, pb-1 to h2-level headings in markdown

--- _md/headingOpen.js
+++ _md/headingOpen.js
@@ -5,9 +5,10 @@ const defaultRender = md.renderer.rules.heading_open || defaultRenderer
 module.exports = function (tokens, idx, options, env, self) {
 	// Get the token
 	const token  = tokens[idx]
+	const classes = token.tag === "h2" ? "mt-3 col-12 border-bottom pb-1 border-dark" : "mt-3 col-12"
 
 	// Add Bootstrap classes
-	token.attrPush(["class", "mt-3 col-12"])
+	token.attrPush(["class", classes])
 
 	// Return the render with the opening wrapper div
 	return defaultRender(tokens, idx, options, env, self)
